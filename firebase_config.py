68d635db049ff23c55c691abe68bb8a022ce2fed
dabouzbrahim4-wq <dabouzbrahim4@gmail.com>
2026-06-11 02:07:59 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,16 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate(
-        "predectivemaintenance-aef92-firebase-adminsdk-fbsvc-511e12fdf5.json"
-    )
+    cred = credentials.Certificate(dict(st.secrets["firebase"]))
 
     firebase_admin.initialize_app(
         cred,
         {
             "databaseURL":
-            "https://predectivemaintenance-aef92-default-rtdb.firebaseio.com/"
+            "https://predictivemaintenance-aef92-default-rtdb.firebaseio.com/"
         }
     )
 
