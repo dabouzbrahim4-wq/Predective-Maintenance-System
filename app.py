ce2f65c12f37c5ef9e4db391e05444f497975e05
dabouzbrahim4-wq <dabouzbrahim4@gmail.com>
2026-06-12 19:11:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -547,7 +547,7 @@ if severity == "Critical":
     machine_status = "Critical"
     status_color = "🔴"
 
-  elif severity == "High و "Medium":
+  elif severity == "High , "Medium":
 
     machine_status = "Warning"
     status_color = "🟡"
