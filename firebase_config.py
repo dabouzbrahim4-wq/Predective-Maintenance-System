856ee929233677a2f43a0e0fe8e22448b9a6116d
dabouzbrahim4-wq <dabouzbrahim4@gmail.com>
2026-06-12 12:11:46 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,11 +1,24 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, db
 
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate(
-        "predectivemaintenance-aef92-firebase-adminsdk-fbsvc-af1df733fc.json"
-    )
+    firebase_credentials = {
+        "type": os.environ["type"],
+        "project_id": os.environ["project_id"],
+        "private_key_id": os.environ["private_key_id"],
+        "private_key": os.environ["private_key"].replace("\\n", "\n"),
+        "client_email": os.environ["client_email"],
+        "client_id": os.environ["client_id"],
+        "auth_uri": os.environ["auth_uri"],
+        "token_uri": os.environ["token_uri"],
+        "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": os.environ["client_x509_cert_url"],
+        "universe_domain": os.environ["universe_domain"]
+    }
+
+    cred = credentials.Certificate(firebase_credentials)
 
     firebase_admin.initialize_app(
         cred,
