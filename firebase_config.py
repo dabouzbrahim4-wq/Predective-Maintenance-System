d4134f02e51bdc3c403855ae8efcf1a12f18d989
dabouzbrahim4-wq <dabouzbrahim4@gmail.com>
2026-06-12 12:31:10 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,30 +1,47 @@
 import os
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
 
 if not firebase_admin._apps:
 
-    firebase_credentials = {
-        "type": os.environ["type"],
-        "project_id": os.environ["project_id"],
-        "private_key_id": os.environ["private_key_id"],
-        "private_key": os.environ["private_key"].replace("\\n", "\n"),
-        "client_email": os.environ["client_email"],
-        "client_id": os.environ["client_id"],
-        "auth_uri": os.environ["auth_uri"],
-        "token_uri": os.environ["token_uri"],
-        "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": os.environ["client_x509_cert_url"],
-        "universe_domain": os.environ["universe_domain"]
-    }
+    try:
+        firebase_credentials = {
+            "type": os.environ["type"],
+            "project_id": os.environ["project_id"],
+            "private_key_id": os.environ["private_key_id"],
+            "private_key": os.environ["private_key"].replace("\\n", "\n"),
+            "client_email": os.environ["client_email"],
+            "client_id": os.environ["client_id"],
+            "auth_uri": os.environ["auth_uri"],
+            "token_uri": os.environ["token_uri"],
+            "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": os.environ["client_x509_cert_url"],
+            "universe_domain": os.environ["universe_domain"]
+        }
+
+    except KeyError:
+
+        firebase_credentials = {
+            "type": st.secrets["type"],
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+            "universe_domain": st.secrets["universe_domain"]
+        }
 
     cred = credentials.Certificate(firebase_credentials)
 
     firebase_admin.initialize_app(
         cred,
         {
-            "databaseURL":
-            "https://predectivemaintenance-aef92-default-rtdb.firebaseio.com/"
+            "databaseURL": "https://predectivemaintenance-aef92-default-rtdb.firebaseio.com/"
         }
     )
 
