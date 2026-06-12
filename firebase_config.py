e94146e0ee75e6716f3724ae83cb420e5bf4cb0c
dabouzbrahim4-wq <dabouzbrahim4@gmail.com>
2026-06-12 10:50:01 +0200

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -4,7 +4,7 @@ from firebase_admin import credentials, db
 if not firebase_admin._apps:
 
     cred = credentials.Certificate(
-        "firebase-key.json"
+        "predectivemaintenance-aef92-firebase-adminsdk-fbsvc-511e12fdf5"
     )
 
     firebase_admin.initialize_app(
